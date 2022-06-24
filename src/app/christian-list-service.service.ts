2fae95e696d556995dd1cf8649e35f6725ba5fbd
christianpinheiro <93pinheiro@gmail.com>
2022-06-24 20:16:49 -0300

    1 file modified

--- src/app/christian-list-service.service.ts
+++ src/app/christian-list-service.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Response {
+
+  created: {
+    rate_float: number;
+  };
+  d1: {
+    updated: string;
+  };
+  d2: {
+    updated: string;
+  };
+}
+
 @Injectable()
 export class ChristianListServiceService {
-  constructor() {}
+  current: Response;
+  list: Array<Response> = [];
+
+  constructor(private http: HttpClient) {}
 }
