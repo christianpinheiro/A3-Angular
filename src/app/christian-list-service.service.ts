061855b3331ce736b966d194717c143eed82701e
christianpinheiro <93pinheiro@gmail.com>
2022-06-24 20:23:11 -0300

    1 file modified

--- src/app/christian-list-service.service.ts
+++ src/app/christian-list-service.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface Response {
-
   created: {
     rate_float: number;
   };
@@ -20,4 +19,17 @@ export class ChristianListServiceService {
   list: Array<Response> = [];
 
   constructor(private http: HttpClient) {}
+
+  update() {
+    if (!this.current) {
+      this.http
+        .get<Response>(
+          'https://archive.org/metadata/TheAdventuresOfTomSawyer_201303'
+        )
+        .subscribe((data) => {
+          this.current = data;
+          this.list.push(data);
+        });
+    }
+  }
 }
